6474fc2ebd33e3e5b19187d9a89476e726804450
yumelih <yumelih@gmail.com>
2024-02-13 21:45:40 +0300

    footer update

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -9,31 +9,38 @@ import StyledText from "../ui/styled-text";
 import Link from "next/link";
 import { Button } from "../ui/button";
 
+const companyItems: string[] = [
+  "About Us",
+  "Contact Us",
+  "Chefs",
+  "Privacy Policy",
+  "Terms & Conditions",
+];
+
+const contactItems: { desc: string; icon: React.ReactElement }[] = [
+  { desc: "123 Street, New York, USA", icon: <MapPinIcon className="w-5" /> },
+  { desc: "+012 345 67890", icon: <PhoneIcon className="w-5" /> },
+  { desc: "mail@domain.com", icon: <EnvelopeIcon className="w-5" /> },
+];
+
+const subItems: string[] = ["Home", "Cookies", "Help", "FAQs"];
+
 function Company() {
   return (
     <div className="flex flex-col gap-2">
       <StyledText text="Company" isDoubleLines={false} />
       <ul>
-        <li className="flex items-center gap-1">
-          <ChevronRightIcon className="w-5" />
-          About Us
-        </li>
-        <li className="flex items-center gap-1">
-          <ChevronRightIcon className="w-5" />
-          Contact Us
-        </li>
-        <li className="flex items-center gap-1">
-          <ChevronRightIcon className="w-5" />
-          Chefs
-        </li>
-        <li className="flex items-center gap-1">
-          <ChevronRightIcon className="w-5" />
-          Privacy Policy
-        </li>
-        <li className="flex items-center gap-1">
-          <ChevronRightIcon className="w-5" />
-          Terms & Condition
-        </li>
+        {companyItems.map((elm) => {
+          return (
+            <li
+              key={elm}
+              className="flex cursor-pointer items-center gap-1 text-white hover:text-gray-200"
+            >
+              <ChevronRightIcon className="w-5" />
+              {elm}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
@@ -44,18 +51,17 @@ function Contact() {
     <div className="flex flex-col gap-2">
       <StyledText text="Contact" isDoubleLines={false} />
       <ul className="space-y-2">
-        <li className="flex items-center gap-2">
-          <MapPinIcon className="w-5" />
-          123 Street, New York, USA
-        </li>
-        <li className="flex items-center gap-2">
-          <PhoneIcon className="w-5" />
-          +012 345 67890
-        </li>
-        <li className="flex items-center gap-2">
-          <EnvelopeIcon className="w-5" />
-          mail@domain.com
-        </li>
+        {contactItems.map((elm) => {
+          return (
+            <li
+              key={elm.desc}
+              className="flex cursor-pointer items-center gap-2 text-white hover:text-gray-200"
+            >
+              {elm.icon}
+              {elm.desc}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
@@ -94,11 +100,17 @@ export default function Footer() {
       </GridAutoFit>
       <div className="mt-12 flex w-full flex-col items-center justify-between gap-2 border-t-2 px-24 pt-4 lg:flex-row lg:gap-0">
         <p>© Ready Chef, All Rights Reserved. Designed By Yumelih</p>
-        <ul className="flex gap-2 divide-x-2 [&_li]:pl-4">
-          <li>Home</li>
-          <li>Cookies</li>
-          <li>Help</li>
-          <li>FAQs</li>
+        <ul className="flex gap-2 divide-x-2 [&_li]:pl-3">
+          {subItems.map((elm) => {
+            return (
+              <li
+                key={elm}
+                className="cursor-pointer text-white hover:text-gray-200"
+              >
+                {elm}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </footer>
